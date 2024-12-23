94eee18bbc428ce0b027cfbfd24dd4b9a363f045
Flmbfle <erwabtot@gmail.com>
2024-12-23 15:02:00 +0100

    🚧 Mise au point avec Benjamin

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Module from './Module';
 import { FaHome, FaBook, FaCalendarAlt, FaCog } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Calendar from './Calendar';
+import Interruptions from './Interruptions';
 
 function App() {
     const [page, setPage] = useState("accueil"); // Gestion des pages
 
     return (
-<div className="container">
+        <div className="container-fluid">
             {/* Navbar */}
             <div className="row">
-                <div className="col-12">
+                    {/* Affichage de la navbar */}
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <a className="navbar-brand" href="#">Gestion des Modules</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,12 +44,13 @@ function App() {
                         </div>
                     </nav>
 
-                    {/* Affichage de la page active */}
-                    {page === "accueil" && <div><h2>Bienvenue sur l'application de gestion de Formation !</h2><p>Choisissez une option dans la navigation pour commencer.</p></div>}
-                    {page === "modules" && <Module />}
-                    {page === "interruptions" && <div>Contenu de la page Interruptions</div>}
-                    {page === "calendrier" && <Calendar />}
-                </div>
+                    <main className='my-3'>
+                        {/* Affichage de la page active */}
+                        {page === "accueil" && <div><h2>Bienvenue sur l'application de gestion de Formation !</h2><p>Choisissez une option dans la navigation pour commencer.</p></div>}
+                        {page === "modules" && <Module />}
+                        {page === "interruptions" && <Interruptions />}
+                        {page === "calendrier" && <Calendar />}
+                    </main>
             </div>
         </div>
     );
