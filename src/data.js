71db121e459ca0c9db84edfcfb5d16f0ef1e6f7c
frc <fcaumartin@gmail.com>
2025-01-06 15:36:51 +0100

    test !!!

--- src/data.js
+++ src/data.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+import { create } from 'zustand'
+
+const useFormationStore = create((set) => ({
+  date_debut: 0,
+  date_fin: 0,
+  nom: "",
+  setDateDebut: () => set((new_date) => ({ date_debut: new_date })),
+}))
 
 
-const textState = atom({
-    key: 'textState', // unique ID (with respect to other atoms/selectors)
-    default: 'toto', // default value (aka initial value)
-  });
-
-
-  export {textState}
+export {useFormationStore}
