ad4f5f7fcc5a34d4e2b81cf4882dc5f54ef881f6
BenjaminClaro <benjamin.claro@free.fr>
2025-01-20 15:23:00 +0100

    fix

--- src/data.js
+++ src/data.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
 import { create } from 'zustand'
+import { persist } from "zustand/middleware";
 
-const useFormationStore = create((set) => ({
-  date_debut: 0,
-  date_fin: 0,
-  nom: "",
-  setDateDebut: () => set((new_date) => ({ date_debut: new_date })),
-  setDateFin: () => set((new_date) => ({ date_fin: new_date })),
-  setNom: () => set((nom) => ({nom})),
-}))
+const useModuleStore = create(
+  persist(
+    (set) => ({
+      modules: [],
+      debut: "",
+      nom: "",
+      setDebut: (debut) => set({ debut }),
+      setNom: (nom) => set({ nom }),
+      addModule: (module) => set((state) => ({ modules: [...state.modules, module] })),
+      updateModule: (index, updatedModule) =>
+        set((state) => {
+          const modules = [...state.modules];
+          modules[index] = updatedModule;
+          return { modules };
+        }),
+      deleteModule: (index) =>
+        set((state) => {
+          const modules = [...state.modules];
+          modules.splice(index, 1);
+          return { modules };
+        }),
+    }),
+    {
+      name: "module-storage",
+    }
+  )
+);
+
 
 const useInterruptionsStore = create((set) => ({
   date_inter: [],
@@ -17,6 +38,6 @@ const useInterruptionsStore = create((set) => ({
 }))
 
 
-export {useFormationStore}
+export default useModuleStore
 export {useInterruptionsStore}
 
